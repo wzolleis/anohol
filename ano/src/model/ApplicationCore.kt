ef997dc67c45e8c5bcacafb578aaf0b08fe20611
Wolfgang Zolleis <wzolleis@web.de>
2021-04-11 19:36:30 +0200

    projekt setup

---
+++ ano/src/model/ApplicationCore.kt
@@ -0,0 +1,4 @@
+package model
+
+data class ApplicationCore(val firstDay: String, val lastDay: String, val workdays: Int) {
+}
---
+++ ano/src/parsen/ParseJsonUseCase.kt
@@ -0,0 +1,10 @@
+package parsen
+
+import model.Application
+
+class ParseJsonUseCase(val contentAsJson: String) {
+    fun start(): Array<String> {
+
+        return arrayOf()
+    }
+}
